a51ef719073c5a149bd0749a2c989294d3414b25
Solomon Bothwell <sbothwell@cj.com>
2020-09-20 23:05:43 -0700

    init

--- default.nix
+++ default.nix
@@ -1,11 +1,11 @@
-{ mkDerivation, base, stdenv }:
+{ mkDerivation, base, simple-affine-space, stdenv }:
 mkDerivation {
   pname = "yampa-playground";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base ];
+  executableHaskellDepends = [ base simple-affine-space ];
   license = "unknown";
   hydraPlatforms = stdenv.lib.platforms.none;
 }
